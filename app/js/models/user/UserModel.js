82b0290be394a1ceece1f4c35c4840ce3fe015b7
Alex Aube <alexandre.desilets-aube.1@ulaval.ca>
2015-11-25 16:11:58 -0500

    added working gravatar

--- app/js/models/user/UserModel.js
+++ app/js/models/user/UserModel.js
@@ -1,10 +1,21 @@
 define([
     'underscore',
-    'backbone'
-], function (_, Backbone) {
+    'backbone',
+    'md5'
+], function (_, Backbone, md5) {
 
     var UserModel = Backbone.Model.extend({
-        urlRoot: 'https://umovie.herokuapp.com/users/'
+        urlRoot: 'https://umovie.herokuapp.com/users/',
+
+        parse: function (response) {
+            response.avatar = this.generateGravatar(response.email);
+            return response;
+        },
+
+        generateGravatar: function (email) {
+            var hashedEmail = md5(email.trim().toLowerCase());
+            return "http://www.gravatar.com/avatar/" + hashedEmail;
+        }
     });
 
     return UserModel;
