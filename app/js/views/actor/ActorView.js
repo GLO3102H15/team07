571201ee6c07c807cf47c17d3d8ffd493e016c95
Jonathan Gingras <jonathan@Jonathans-MacBook-Pro.local>
2015-11-27 16:59:28 -0500

    fixing broken actor api

--- app/js/views/actor/ActorView.js
+++ app/js/views/actor/ActorView.js
@@ -35,18 +35,32 @@ define([
             var movieDbApiKey = "?api_key=9ab7462331ecc3ac8351a0c1cb87eeb0";
             var movieDbApiImgPath = "https://image.tmdb.org/t/p/original";
             var actorNameQuery = "&query=" + this.model.attributes.artistName.split(' ').join('+');
-            $.get(movieDbApiUrl + "/search/person" + movieDbApiKey + actorNameQuery).done(function (data) {
-                if(data.results[0]) {
-                    if (data.results[0].profile_path) {
-                        document.getElementById("actor-pic").src = movieDbApiImgPath + data.results[0].profile_path;
-                        document.getElementById("actorBackground").setAttribute("style", "background-image: url(" + movieDbApiImgPath + data.results[0].profile_path + ")");
-                    }
-                    if (data.results[0].id) {
-                        $.get(movieDbApiUrl + "/person/" + data.results[0].id + movieDbApiKey).done(function (data) {
-                            var bio = document.createElement("li");
-                            bio.textContent = data.biography;
-                            document.getElementById("actor-info").appendChild(bio);
-                        });
+            $.ajax({
+                type: "GET",
+                url: movieDbApiUrl + "/search/person" + movieDbApiKey + actorNameQuery,
+                dataType: "jsonp",
+                jsonpCallback: 'test',
+                contentType: 'application/json',
+                success: function(data) {
+                    if (data.results[0]) {
+                        if (data.results[0].profile_path) {
+                            document.getElementById("actor-pic").src = movieDbApiImgPath + data.results[0].profile_path;
+                            document.getElementById("actorBackground").setAttribute("style", "background-image: url(" + movieDbApiImgPath + data.results[0].profile_path + ")");
+                        }
+                        if (data.results[0].id) {
+                            $.ajax({
+                                type: "GET",
+                                url: movieDbApiUrl + "/person/" + data.results[0].id + movieDbApiKey,
+                                dataType: "jsonp",
+                                jsonpCallback: 'test',
+                                contentType: 'application/json',
+                                success: function (bioData) {
+                                    var bio = document.createElement("li");
+                                    bio.textContent = bioData.biography;
+                                    document.getElementById("actor-info").appendChild(bio);
+                                }
+                            });
+                        }
                     }
                 }
             });
